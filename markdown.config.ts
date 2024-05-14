e2427ae340e437797724898cf7f8aabc46e65201
Frank Mayer <gh@frankmayer.dev>
2024-05-14 18:50:01 +0200

    remove toc

--- markdown.config.ts
+++ markdown.config.ts
@@ -1,23 +1,5 @@
-import remarkToc from "remark-toc"
-import rehypeToc from "rehype-toc"
-import rehypeSlug from "rehype-slug"
-
 export default {
     gfm: true,
     syntaxHighlight: "shiki",
     shikiConfig: { wrap: false, theme: "catppuccin-mocha" },
-    remarkPlugins: [[remarkToc, { tight: true, ordered: true }]],
-    rehypePlugins: [
-        rehypeSlug,
-        [
-            rehypeToc,
-            {
-                headings: ["h1", "h2", "h3"],
-                cssClasses: {
-                    toc: "toc-post",
-                    link: "toc-link",
-                },
-            },
-        ],
-    ],
 }
