af4009dc2204f041f9570b170038f9f94efd2df7
tsukinoko-kun <tsukinoko-kun@users.noreply.github.com>
2024-05-29 20:39:06 +0000

    Apply formatting changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,23 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import markdownConfig from "./markdown.config";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config"
+import mdx from "@astrojs/mdx"
+import sitemap from "@astrojs/sitemap"
+import markdownConfig from "./markdown.config"
+import tailwind from "@astrojs/tailwind"
 
-import solidJs from "@astrojs/solid-js";
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://thinkofit.blog",
-  integrations: [mdx({
-    ...markdownConfig,
-    extendPlugins: false
-  }), sitemap(), tailwind(), solidJs()],
-  markdown: markdownConfig,
-  markdown: markdownConfig
-});
+    site: "https://thinkofit.blog",
+    integrations: [
+        mdx({
+            ...markdownConfig,
+            extendPlugins: false,
+        }),
+        sitemap(),
+        tailwind(),
+        solidJs(),
+    ],
+    markdown: markdownConfig,
+    markdown: markdownConfig,
+})
