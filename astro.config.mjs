cde77dbb04ebbe4c071a37274c0684595ee42ecb
Frank Mayer <gh@frankmayer.dev>
2024-05-29 15:12:03 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,16 @@
-import { defineConfig } from "astro/config"
-import mdx from "@astrojs/mdx"
-import sitemap from "@astrojs/sitemap"
-import markdownConfig from "./markdown.config"
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import markdownConfig from "./markdown.config";
+import tailwind from "@astrojs/tailwind";
 
+// https://astro.build/config
 export default defineConfig({
-    site: "https://thinkofit.blog",
-    integrations: [
-        mdx({
-            ...markdownConfig,
-            extendPlugins: false,
-        }),
-        sitemap(),
-        tailwind(),
-    ],
-    markdown: markdownConfig,
-
-    markdown: markdownConfig,
-})
+  site: "https://thinkofit.blog",
+  integrations: [mdx({
+    ...markdownConfig,
+    extendPlugins: false
+  }), sitemap(), tailwind()],
+  markdown: markdownConfig,
+  markdown: markdownConfig
+});
