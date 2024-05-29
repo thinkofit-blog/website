24fae343224aa0ce20ecc19043472bc5c7028ffb
Frank Mayer <gh@frankmayer.dev>
2024-05-29 22:32:26 +0200

    fix build error

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,43 +1,22 @@
-import { initializeApp, type FirebaseApp } from "firebase/app"
-import {
-    getAuth,
-    GoogleAuthProvider,
-    GithubAuthProvider,
-    signInWithPopup,
-    type Auth,
-    type UserCredential,
-    type User,
-    signInWithRedirect,
-} from "firebase/auth"
-import { createComputed, createEffect, createSignal } from "solid-js"
+import { initializeApp } from "firebase/app"
+import { getAuth, GoogleAuthProvider, GithubAuthProvider, signInWithRedirect } from "firebase/auth"
 
-type FB = {
-    readonly app: FirebaseApp
-    readonly googleAuth: GoogleAuthProvider
-    readonly githubAuth: GithubAuthProvider
-    readonly auth: Auth
+const firebaseConfig = {
+    apiKey: "AIzaSyB-0b4L0WNtHr6hQIzHSaUrWkD-uBloWao",
+    authDomain: "think-of-it.firebaseapp.com",
+    projectId: "think-of-it",
+    storageBucket: "think-of-it.appspot.com",
+    messagingSenderId: "243778068084",
+    appId: "1:243778068084:web:5b8c9b7ffea1f4ba6e3b21",
 }
 
-function initFirebase(): FB {
-    const firebaseConfig = {
-        apiKey: "AIzaSyB-0b4L0WNtHr6hQIzHSaUrWkD-uBloWao",
-        authDomain: "think-of-it.firebaseapp.com",
-        projectId: "think-of-it",
-        storageBucket: "think-of-it.appspot.com",
-        messagingSenderId: "243778068084",
-        appId: "1:243778068084:web:5b8c9b7ffea1f4ba6e3b21",
-    }
+const app = initializeApp(firebaseConfig)
+const auth = getAuth(app)
 
-    const app = initializeApp(firebaseConfig)
-    const auth = getAuth(app)
+const googleAuth = new GoogleAuthProvider()
+const githubAuth = new GithubAuthProvider()
 
-    const googleAuth = new GoogleAuthProvider()
-    const githubAuth = new GithubAuthProvider()
-
-    return { app, googleAuth, githubAuth, auth }
-}
-
-const fb = initFirebase()
+const fb = { app, googleAuth, githubAuth, auth }
 
 export function Comments() {
     if (fb.auth.currentUser) {
