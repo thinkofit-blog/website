6e3c2354d2c8e9811857070abe3e83265c70f7b2
Frank <gh@frankmayer.dev>
2024-05-30 17:21:11 +0200

    Comments (#10)

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -4,12 +4,52 @@ import { createSignal, onMount } from "solid-js"
 
 const requestUrl = new URL(location.href)
 
+const articleId = requestUrl.pathname.split("/").filter(Boolean).pop() || requestUrl.pathname
+
 const supabase = createClient(
     "https://kzqpqtqdmhjmabwsvlts.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt6cXBxdHFkbWhqbWFid3N2bHRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTcwMTcwNzUsImV4cCI6MjAzMjU5MzA3NX0.aZ-f4rlHMapYuFk3C_92KZnOyxSbGGIgr0bCm5MdV_w",
 )
 
+const userRegistry = new Map<bigint, { name: string; avatar: string }>()
+
+async function getUserFromReg(userId: bigint) {
+    if (!userRegistry.has(userId)) {
+        const userResp = await supabase.from("users").select("name, avatar").filter("id", "eq", userId).single()
+        if (userResp.error) {
+            console.error(userResp.error)
+            userRegistry.set(userId, { name: "N/A", avatar: "" })
+        } else {
+            userRegistry.set(userId, userResp.data)
+        }
+    }
+    return userRegistry.get(userId)!
+}
+
+function getUserName(user: User): string {
+    return (
+        user.user_metadata?.preferred_username ||
+        user.user_metadata?.name ||
+        user.user_metadata?.user_name ||
+        user.user_metadata?.email ||
+        user.user_metadata?.full_name ||
+        user.email ||
+        "N/A"
+    )
+}
+
+function getUserAvatar(user: User): string {
+    return user.user_metadata.avatar_url || user.user_metadata.picture || ""
+}
+
 ;(globalThis as any).supabase = supabase
+
+type comment = {
+    created_at: Date
+    userName: string
+    userAvatar: string
+    content: string
+}
 
 async function tryLogin(searchParams: URLSearchParams) {
     const access_token = searchParams.get("access_token")
@@ -45,7 +85,9 @@ const signInWithGithub = signInWith("github")
 const signInWithLinkedin = signInWith("linkedin_oidc")
 
 export const Comments = () => {
-    const [user, setUser] = createSignal<User | null>(null)
+    const [getUser, setUser] = createSignal<User | null>(null)
+    const [getComments, setComments] = createSignal<comment[]>([])
+    const [getComment, setComment] = createSignal<string>("")
 
     onMount(async () => {
         const {
@@ -56,6 +98,31 @@ export const Comments = () => {
         supabase.auth.onAuthStateChange((_event, session: AuthSession | null) => {
             setUser(session?.user ?? null)
         })
+
+        const { data: comments, error: err } = await supabase
+            .from("comments")
+            .select("created_at, user, post, content")
+            .filter("post", "eq", articleId)
+            .order("created_at", { ascending: false })
+            .limit(64)
+        if (err) {
+            console.error(err)
+        }
+        if (comments) {
+            setComments(
+                await Promise.all(
+                    comments.map(async (x) => {
+                        const u = await getUserFromReg(x.user)
+                        return {
+                            created_at: x.created_at,
+                            userName: u.name,
+                            userAvatar: u.avatar,
+                            content: x.content,
+                        }
+                    }),
+                ),
+            )
+        }
     })
 
     async function signOut() {
@@ -65,31 +132,114 @@ export const Comments = () => {
 
     return (
         <div class="rounded-md border border-ctp-overlay0 p-2">
-            {user() ? (
-                <div class="group flex flex-row flex-wrap items-center justify-between gap-2">
-                    <img
-                        class="block aspect-square h-10 w-10"
-                        src={user()!.user_metadata.avatar_url ?? user()!.user_metadata.picture ?? ""}
-                        alt=""
-                        width="50"
-                        loading="lazy"
-                        decoding="async"
-                    />
-                    <span class="block h-fit text-sm">
-                        {user()!.user_metadata?.preferred_username ||
-                            user()!.user_metadata?.name ||
-                            user()!.user_metadata?.user_name ||
-                            user()!.user_metadata?.email ||
-                            user()!.user_metadata?.full_name ||
-                            user()!.email}
-                    </span>
+            {getUser() ? (
+                <>
+                    <div class="group flex flex-row flex-wrap items-center justify-between gap-2">
+                        <img
+                            class="block aspect-square h-10 w-10 rounded-md"
+                            src={getUserAvatar(getUser()!)}
+                            alt=""
+                            width="50"
+                            loading="lazy"
+                            decoding="async"
+                        />
+                        <span class="block h-fit text-sm">{getUserName(getUser()!)}</span>
+                        <button
+                            class="block h-fit rounded-md bg-ctp-red px-2 py-1 text-sm text-ctp-base md:invisible md:group-hover:visible"
+                            onClick={signOut}
+                        >
+                            Abmelden
+                        </button>
+                    </div>
+                    <label class="w-full">
+                        Schreibe ein Kommentar {getComment() ? `(${getComment().length}/280)` : null}
+                        <br />
+                        <textarea
+                            value={getComment()}
+                            class="h-24 w-full resize-none rounded-md bg-ctp-crust px-2 py-1 text-ctp-text"
+                            maxLength={280}
+                            onInput={(x) => {
+                                setComment(x.currentTarget.value)
+                            }}
+                        />
+                    </label>
                     <button
-                        class="block h-fit rounded-md bg-ctp-red px-2 py-1 text-sm text-ctp-base md:invisible md:group-hover:visible"
-                        onClick={signOut}
+                        class="ml-auto block rounded-md bg-ctp-text px-2 py-1 text-ctp-base"
+                        disabled={getComment().length === 0}
+                        onClick={async () => {
+                            const userResp = await supabase.from("users").upsert(
+                                {
+                                    user_id: getUser()!.id,
+                                    name: getUserName(getUser()!),
+                                    avatar: getUserAvatar(getUser()!),
+                                },
+                                { onConflict: "user_id" },
+                            )
+                            if (userResp.error) {
+                                console.error(userResp.error)
+                                alert(
+                                    userResp.statusText + "\n" + userResp.error.message ||
+                                        JSON.stringify(userResp.error),
+                                )
+                                return
+                            }
+
+                            const userSelectResp = await supabase
+                                .from("users")
+                                .select("id, name, avatar")
+                                .filter("user_id", "eq", getUser()!.id)
+                                .limit(1)
+
+                            if (userSelectResp.error) {
+                                console.error(userSelectResp.error)
+                                alert(
+                                    userSelectResp.statusText + "\n" + userSelectResp.error.message ||
+                                        JSON.stringify(userSelectResp.error),
+                                )
+                                return
+                            }
+                            if (userSelectResp.data?.length !== 1) {
+                                console.error("failed to get user data")
+                                alert("fehler beim abfragen der Nutzerdaten")
+                                return
+                            }
+
+                            userRegistry.set(userSelectResp.data[0]!.id, {
+                                name: userSelectResp.data[0]!.name,
+                                avatar: userSelectResp.data[0]!.avatar,
+                            })
+
+                            const commentResp = await supabase.from("comments").insert({
+                                user_id: getUser()!.id,
+                                post: articleId,
+                                content: getComment(),
+                                user: userSelectResp.data[0]!.id,
+                            })
+                            if (commentResp.error) {
+                                console.error(commentResp.error)
+                                alert(
+                                    commentResp.statusText + "\n" + commentResp.error.message ||
+                                        JSON.stringify(commentResp.error),
+                                )
+                                return
+                            }
+
+                            setComments([
+                                {
+                                    created_at: new Date(),
+                                    userName: getUserName(getUser()!),
+                                    userAvatar: getUserAvatar(getUser()!),
+                                    content: getComment(),
+                                },
+                                ...getComments(),
+                            ])
+
+                            setComment("")
+                        }}
                     >
-                        Abmelden
+                        Absenden
                     </button>
-                </div>
+                </>
             ) : (
                 <div>
                     <p>Melde dich an, um einen Kommentar auf diesem Artikel zu hinterlassen.</p>
@@ -116,6 +266,27 @@ export const Comments = () => {
                     </ul>
                 </div>
             )}
+            {getComments().length ? (
+                <ul class="mt-4 border-t border-t-ctp-overlay0 pt-2">
+                    {getComments().map((comment) => {
+                        return (
+                            <li class="my-1">
+                                <div class="flex flex-row">
+                                    <img
+                                        src={comment.userAvatar ?? ""}
+                                        alt=""
+                                        loading="lazy"
+                                        decoding="async"
+                                        class="block aspect-square h-6 w-6 rounded-md"
+                                    />
+                                    <span>{comment.userName}</span>
+                                </div>
+                                <p class="block">{comment.content}</p>
+                            </li>
+                        )
+                    })}
+                </ul>
+            ) : null}
         </div>
     )
 }
