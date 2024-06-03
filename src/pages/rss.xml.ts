23c6cdf3cbdab2e6969c6e52649d3e1cc6b6bc0a
Frank Mayer <gh@frankmayer.dev>
2024-06-03 18:45:16 +0200

    auto generate bot files

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,13 +1,14 @@
+import type { APIRoute } from "astro"
 import rss from "@astrojs/rss"
 import { getCollection } from "astro:content"
 import { SITE_TITLE, SITE_DESCRIPTION } from "../consts"
 
-export async function GET(context) {
+export const GET: APIRoute = async (context) => {
     const posts = await getCollection("articles")
     return rss({
         title: SITE_TITLE,
         description: SITE_DESCRIPTION,
-        site: context.site,
+        site: context.site || "/404",
         items: posts.map((post) => ({
             ...post.data,
             link: `/articles/${post.slug}/`,
