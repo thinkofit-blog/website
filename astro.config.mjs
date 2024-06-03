5b715634e2d488241b8ec462238a293922a54654
Frank <gh@frankmayer.dev>
2024-06-03 18:46:01 +0200

    auto generate bot files (#20)

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from "@astrojs/mdx"
 import sitemap from "@astrojs/sitemap"
 import markdownConfig from "./markdown.config"
 import tailwind from "@astrojs/tailwind"
-
 import solidJs from "@astrojs/solid-js"
+import { join } from "node:path"
+import { readFileSync } from "node:fs"
 
-// https://astro.build/config
+const pubDateRE = /^\s*pubDate\s*:\s*["']?([A-Za-z0-9.\s]+)["']?/m
+
 export default defineConfig({
     site: "https://thinkofit.blog",
     integrations: [
@@ -14,7 +16,27 @@ export default defineConfig({
             ...markdownConfig,
             extendPlugins: false,
         }),
-        sitemap(),
+        sitemap({
+            serialize(item) {
+                item.lastmod ||= new Date()
+                item.changefreq = "monthly"
+                item.priority = 0.75
+                if (/authors/.test(item.url)) {
+                    item.priority = 0.5
+                    item.lastmod = new Date("Jun 03 2024")
+                } else if (/articles/.test(item.url)) {
+                    const filename = join("./src/content/", new URL(item.url).pathname.replace(/\/$/, ".md"))
+                    const fileContent = readFileSync(filename, "utf8")
+                    const reArr = pubDateRE.exec(fileContent)
+                    if (reArr && reArr.length) {
+                        item.lastmod = new Date(reArr[1])
+                    }
+
+                    item.priority = 1
+                }
+                return item
+            },
+        }),
         tailwind(),
         solidJs(),
     ],
